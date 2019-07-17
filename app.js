9b1a6f58f7ab56571daf1623075125a8bb392ebf
Matti <matti94@live.se>
2019-07-17 18:52:32 +0200

    Commit 6

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var spelare = [];
 var id = 0;
+//var spelare = [{name:"JP",init:12,id:id++ }];
+var spelare = JSON.parse(localStorage.getItem("Sparfil") || "[]");
 function save() {
 
 
@@ -13,12 +14,19 @@ function save() {
    spelare.push({
        name:$name.value , init:$init.value, id:id++
    })
+   localStorage.setItem("Sparfil", JSON.stringify(spelare));
  
    //tömmer värden på data
    $init.value = "";
    $name.value = "";
 
    //uppdatera listan på skärmen
+   uppdatera();
+}
+
+function submit(e) {
+    e.preventDefault();
+    save();
 }
 
 function sort() {
@@ -30,17 +38,20 @@ function uppdatera ()
 {
     var listan = document.getElementById("spelarna");
     console.log(listan.childElementCount);
+    //tömmer listan
     if (listan.childElementCount > 0)
     {
         document.getElementById("spelarna").innerHTML = "";
     }
-    var li = document.createElement("li");
+    
     for (var i = 0; i < spelare.length ; i++ )
     {
+        var li = document.createElement("li");
         var temp = spelare[i];
         //fixa så att det ser bra ut i listan
-        li.appendChild(document.createTextNode(JSON.stringify(temp)));
+        li.appendChild(document.createTextNode(`${temp.name}: ${temp.init}`));
         li.setAttribute("id", "element"+i);
         listan.appendChild(li);
     }
 }
+uppdatera();
